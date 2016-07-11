725081521afe9259eaac3cce3017134399a14674
Daniel Seijo Sánchez <daniseijo12@gmail.com>
2016-07-11 21:15:19 +0200

    Added some security with a simple hash check.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express'),
     methodOverride = require('method-override'),
     mongoose = require('mongoose');
 
+const ROOT_KEY = '79edc86c9b2930aecdfcf395ffb695a0';
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -33,47 +35,67 @@ app.use(router);
 
 var api = express.Router();
 
+function isAuthenticated(req, res, next) {
+    var auth = req.get('rootAuth');
+    if (auth == ROOT_KEY) {
+        return next();
+    }
+    
+    return res.status(401).send('HTTP Error 401 - Unauthorized: Access is denied due to invalid credentials.');
+}
+
 // Routes for Application
 api.route('/application')
+    .all(isAuthenticated)
     .get(ApplicationCtrl.findAllApplications)
     .post(ApplicationCtrl.addApplication);
 
 api.route('/application/beacon/:uuid')
+    .all(isAuthenticated)
     .get(ApplicationCtrl.findByUUID);
 
 api.route('/application/:id')
+    .all(isAuthenticated)
     .get(ApplicationCtrl.findById)
     .put(ApplicationCtrl.updateApplication)
     .delete(ApplicationCtrl.deleteApplication);
 
 // Routes for Place
 api.route('/place')
+    .all(isAuthenticated)
     .get(PlaceCtrl.findAllPlaces)
     .post(PlaceCtrl.addPlace);
 
 api.route('/place/beacon/:uuid/:major')
+    .all(isAuthenticated)
     .get(PlaceCtrl.findByUUIDMajor);
 
 api.route('/place/beacon/:uuid')
+    .all(isAuthenticated)
     .get(PlaceCtrl.findByUUID);
 
 api.route('/place/:id')
+    .all(isAuthenticated)
     .get(PlaceCtrl.findById)
     .put(PlaceCtrl.updatePlace)
     .delete(PlaceCtrl.deletePlace);
 
 // Routes for Element
 api.route('/element')
+    .all(isAuthenticated)
     .get(ElementCtrl.findAllElements)
     .post(ElementCtrl.addElement);
 
 api.route('/element/beacon/:uuid/:major')
+    .all(isAuthenticated)
     .get(ElementCtrl.findByUUIDMajor);
 
 api.route('/element/beacon/:uuid/:major/:minor')
+    .all(isAuthenticated)
     .get(ElementCtrl.findByUUIDMajorMinor);
 
 api.route('/element/:id')
+    .all(isAuthenticated)
     .get(ElementCtrl.findById)
     .put(ElementCtrl.updateElement)
     .delete(ElementCtrl.deleteElement);
